fd40fa07216d3b1c61dc7b744ac7827ed1e5488f
mr.duan <861272006@qq.com>
2023-02-01 00:11:30 +0800

    xlsx 查找sheet

--- day11/test_list.py
+++ day11/test_list.py
@@ -2,12 +2,16 @@
 Author: mr.duan 861272006@qq.com
 Date: 2023-01-31 00:26:59
 LastEditors: mr.duan 861272006@qq.com
-LastEditTime: 2023-01-31 00:31:07
+LastEditTime: 2023-01-31 21:31:40
 FilePath: \learn\python\day11\test_list.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 from openpyxl import load_workbook
 
-workbook = load_workbook(filename = "D:/learn/python/day11/test.xlsx")
+workbook = load_workbook(filename = "D:/learn/python/day11/Dera SSD_01.xlsx")
 
-print(workbook.sheetnames)
+#获取sheet名称
+#print(workbook.sheetnames)
+
+sheet = workbook["Sheet1"]
+print(sheet)
