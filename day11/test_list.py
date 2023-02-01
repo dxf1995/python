3c2ef32660f1ba403bfabcf3f3c68143ec5d8c3d
mr.duan <861272006@qq.com>
2023-02-01 14:00:24 +0800

    提供学习 .xlsx表格

--- day11/test_list.py
+++ day11/test_list.py
@@ -1,17 +1,10 @@
-'''
-Author: mr.duan 861272006@qq.com
-Date: 2023-01-31 00:26:59
-LastEditors: mr.duan 861272006@qq.com
-LastEditTime: 2023-01-31 21:31:40
-FilePath: \learn\python\day11\test_list.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 from openpyxl import load_workbook
 
-workbook = load_workbook(filename = "D:/learn/python/day11/Dera SSD_01.xlsx")
+workbook = load_workbook(filename = "D:/learn/python/day11/CPU详细参数汇总.xlsx")
 
 #获取sheet名称
-#print(workbook.sheetnames)
+print(workbook.sheetnames)
 
-sheet = workbook["Sheet1"]
+sheet = workbook["志强3代Icelake"]
 print(sheet)
